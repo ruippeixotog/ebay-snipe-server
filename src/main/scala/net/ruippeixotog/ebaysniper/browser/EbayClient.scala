0989195e1bc9345c6d35c83af5813b1a2995e1d4
Rui Gonçalves <ruippeixotog@gmail.com>
2014-09-28 13:08:12 +0100

    Create a "native" ebay client with auction info support

---
+++ src/main/scala/net/ruippeixotog/ebaysniper/browser/EbayClient.scala
@@ -0,0 +1,64 @@
+package net.ruippeixotog.ebaysniper.browser
+
+import com.github.nscala_time.time.Imports._
+import com.jbidwatcher.util.Currency
+import com.typesafe.config.ConfigFactory
+import net.ruippeixotog.ebaysniper.browser.Browser._
+import net.ruippeixotog.ebaysniper.model.{Auction, Seller}
+
+import scala.reflect.ClassTag
+
+class EbayClient(site: String, username: String, password: String) extends BiddingClient {
+  implicit val browser = new Browser
+
+  val siteConfig =
+    ConfigFactory.load.getConfig(s"ebay.sites-config.${site.replace('.', '-')}").
+      withFallback(ConfigFactory.parseString(s"name = $site"))
+
+  val loginMgr = new EbayLoginManager(siteConfig, username, password)
+
+  def login() = loginMgr.forceLogin()
+
+  def auctionURL(auctionId: String) =
+    siteConfig.getString("protocol") + siteConfig.getString("view-host") +
+        siteConfig.getString("file") + '?' + siteConfig.getString("view-cmd") +
+        siteConfig.getString("view-cgi") + auctionId
+
+  def auctionInfo(auctionId: String): Auction = {
+    val auctionHtml = browser.get(auctionURL(auctionId))
+    val aiConfig = siteConfig.getConfig("auction-info")
+
+    val strippedElems = auctionHtml.select("head, #LeftSummaryPanel, #RightSummaryPanel")
+
+    def query[T: ClassTag](attr: String) =
+      strippedElems.selectFromConfig(aiConfig.getConfig(attr)).asInstanceOf[Option[T]]
+
+    val currentBid = Currency.getCurrency(query[String]("current-bid").get)
+
+    val shippingCost = query[String]("shipping-cost") match {
+      case None | Some("FREE") => Currency.getCurrency(currentBid.getCurrencySymbol, 0.0)
+      case Some(price) => Currency.getCurrency(price)
+    }
+
+    val buyNowPrice = query[String]("buy-now-price") match {
+      case None => null
+      case Some(price) => Currency.getCurrency(price)
+    }
+
+    Auction(auctionId,
+      title = query[String]("title").get,
+      endingAt = query[DateTime]("ending-at").get.withZone(DateTimeZone.getDefault()),
+      seller = Seller(
+        id = query[String]("seller.id").get,
+        feedback = query[String]("seller.feedback").fold(0)(_.toInt),
+        positivePercentage = query[String]("seller.positive-percentage").fold(100.0)(_.toDouble)),
+      currentBid = currentBid,
+      bidCount = query[String]("bid-count").get.toInt,
+      buyNowPrice = buyNowPrice,
+      location = query[String]("location").get,
+      shippingCost = shippingCost,
+      thumbnailUrl = query[String]("thumbnail-url").get)
+  }
+
+  def bid(auctionId: String, bid: Currency, quantity: Int) = ???
+}
---
+++ src/main/scala/net/ruippeixotog/ebaysniper/browser/EbayLoginManager.scala
@@ -0,0 +1,58 @@
+package net.ruippeixotog.ebaysniper.browser
+
+import com.typesafe.config.Config
+import net.ruippeixotog.ebaysniper.browser.Browser._
+import net.ruippeixotog.ebaysniper.util.Logging
+import org.jsoup.nodes.Document
+
+import scala.collection.convert.WrapAsScala._
+
+class EbayLoginManager(siteConf: Config, username: String, password: String)(
+    implicit browser: Browser) extends Logging {
+  
+  val loginConf = siteConf.getConfig("login")
+
+  def login(): Boolean = {
+    if(browser.cookies.contains("shs")) true
+    else forceLogin()
+  }
+
+  def forceLogin(): Boolean = {
+    browser.cookies.clear()
+    log.info("Getting the sign in cookie for {}", siteConf.getString("name"))
+
+    val signInHtml = browser.get(loginConf.getString("sign-in-page"))
+    val signInForm = signInHtml.select("form").filter(_.select("input[name=pass]").nonEmpty).head
+
+    val signInData = signInForm.extractFormData + ("userid" -> username) + ("pass" -> password)
+    val confirmHtml = browser.post(signInForm.attr("action"), signInData)
+
+    val isSuccess = validLogin(confirmHtml)
+    if (isSuccess) {
+      val hidUrl = confirmHtml.select("form input[name=hidUrl]")(0).attr("value")
+
+      if (!hidUrl.matches(loginConf.getString("valid-success-uri")))
+        log.warn("Security checks out, but no My eBay form link on final page")
+
+      log.info("Login successful")
+    }
+
+    isSuccess
+  }
+
+  private[this] def validLogin(doc: Document): Boolean = {
+    if(doc.baseUri.contains("FYPShow") || doc.title == "Reset your password") {
+      log.error("eBay is requesting that you change your password. You must change your password on eBay.")
+      false
+    }
+    else if (doc.egrep(loginConf.getString("verification-needed")).nonEmpty) {
+      log.error("eBay's security monitoring has been triggered, and temporarily requires human " +
+          "intervention to log in")
+      false
+    }
+    else if (doc.egrep(loginConf.getString("invalid-credentials")).nonEmpty) {
+      log.error("Your sign in information is not correct")
+      false
+    } else true
+  }
+}
