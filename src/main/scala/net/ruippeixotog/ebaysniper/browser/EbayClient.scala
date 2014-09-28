0adadaab019740e21faca72a078820a919e5e332
Rui Gonçalves <ruippeixotog@gmail.com>
2014-09-28 18:48:04 +0100

    Reorganize config keys

--- src/main/scala/net/ruippeixotog/ebaysniper/browser/EbayClient.scala
+++ src/main/scala/net/ruippeixotog/ebaysniper/browser/EbayClient.scala
@@ -21,18 +21,18 @@ class EbayClient(site: String, username: String, password: String) extends Biddi
 
   def auctionURL(auctionId: String) =
     siteConfig.getString("protocol") + siteConfig.getString("view-host") +
-        siteConfig.getString("file") + '?' + siteConfig.getString("view-cmd") +
-        siteConfig.getString("view-cgi") + auctionId
+        siteConfig.getString("file") + '?' + siteConfig.getString("auction-info.uri-cmd") +
+        siteConfig.getString("auction-info.uri-cgi") + auctionId
 
   def auctionInfo(auctionId: String): Auction = {
     val auctionHtml = browser.get(auctionURL(auctionId))
-    val aiConfig = siteConfig.getConfig("auction-info")
 
-    val strippedElems = auctionHtml.select("head, #LeftSummaryPanel, #RightSummaryPanel, " +
-        "#CenterPanelInternal")
+    val aiConfig = siteConfig.getConfig("auction-info")
+    val attrsConfig = aiConfig.getConfig("attributes")
+    val contentElems = auctionHtml.select(aiConfig.getString("content-query"))
 
     def query[T: ClassTag](attr: String): Option[T] =
-      strippedElems.selectFromConfig(aiConfig.getConfig(attr)).asInstanceOf[Option[T]]
+      contentElems.selectFromConfig(attrsConfig.getConfig(attr)).asInstanceOf[Option[T]]
 
     val endingAt = query[DateTime]("ending-at").fold[DateTime](new DateTime(0))(
       _.withZone(DateTimeZone.getDefault()))
