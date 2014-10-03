7f8f27cf78d22cbc28fa30feb2313aad367cd1f3
Rui Gonçalves <ruippeixotog@gmail.com>
2014-10-03 22:04:20 +0100

    Use the new EbayClient in the SnipeServer

--- src/main/scala/net/ruippeixotog/ebaysniper/SnipeServer.scala
+++ src/main/scala/net/ruippeixotog/ebaysniper/SnipeServer.scala
@@ -3,14 +3,12 @@ package net.ruippeixotog.ebaysniper
 import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
 import net.ruippeixotog.ebaysniper.JsonProtocol._
-import net.ruippeixotog.ebaysniper.browser.{JBidwatcherClient, BiddingClient}
+import net.ruippeixotog.ebaysniper.browser.EbayClient
 import net.ruippeixotog.ebaysniper.util.RoutingLogging
 import spray.http.StatusCodes.{Success => _, _}
 import spray.httpx.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
-import spray.json._
 import spray.routing.SimpleRoutingApp
-import net.ruippeixotog.ebaysniper.JsonProtocol._
 
 object SnipeServer extends App with SimpleRoutingApp with RoutingLogging with SnipeManagement {
 
@@ -26,7 +24,7 @@ object SnipeServer extends App with SimpleRoutingApp with RoutingLogging with Sn
   implicit def executionContext = system.dispatcher
 
   // create the eBay server interface
-  implicit val ebay = new JBidwatcherClient(site, username, password)
+  implicit val ebay = new EbayClient(site, username, password)
   ebay.login()
 
   override val snipesFile = Option(config.getString("sniper.snipes-file"))
