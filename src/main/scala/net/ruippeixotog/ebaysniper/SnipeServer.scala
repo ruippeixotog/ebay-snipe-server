4e0f542bdfc06f500b4de1831d9a56c58b5bf285
Rui Gonçalves <ruippeixotog@gmail.com>
2015-04-12 20:33:08 +0100

    Gracefully handle errors coming from eBay servers

--- src/main/scala/net/ruippeixotog/ebaysniper/SnipeServer.scala
+++ src/main/scala/net/ruippeixotog/ebaysniper/SnipeServer.scala
@@ -7,10 +7,12 @@ import com.typesafe.config.ConfigFactory
 import net.ruippeixotog.ebaysniper.JsonProtocol._
 import net.ruippeixotog.ebaysniper.ebay.EbayClient
 import net.ruippeixotog.ebaysniper.util.RoutingLogging
+import org.jsoup.HttpStatusException
 import spray.http.StatusCodes.{Success => _, _}
 import spray.httpx.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
-import spray.routing.SimpleRoutingApp
+import spray.routing.{ExceptionHandler, SimpleRoutingApp}
+import spray.util.LoggingContext
 
 object SnipeServer extends App with SimpleRoutingApp with RoutingLogging with SnipeManagement {
 
@@ -34,47 +36,65 @@ object SnipeServer extends App with SimpleRoutingApp with RoutingLogging with Sn
   override val snipesFile = Option(new File(config.getString("sniper.snipes-file")))
   loadSnipesFromFile()
 
-  startServer("0.0.0.0", config.getInt("sniper.port")) {
-    logServiceRequest {
-      pathPrefix("auction" / Segment) { auctionId =>
-        path("snipe") {
-          get {
-            complete {
-              snipes.get(auctionId) match {
-                case None => NotFound -> "No snipe was defined for this auction yet."
-                case Some(snipe) => snipe.info
-              }
-            }
-          } ~
-          post {
-            entity(as[SnipeInfo]) { reqInfo =>
-              complete {
-                scheduler.snipeTimeFor(auctionId, reqInfo.snipeTime) match {
-                  case None => BadRequest -> "The auction has already ended."
+  def ebayHttpExceptionHandler(implicit log: LoggingContext) = ExceptionHandler {
+    case e: HttpStatusException => ctx => {
+      log.warning("HTTP {}: {}", e.getStatusCode, e.getUrl)
+      e.getStatusCode match {
+        case 404 =>
+          ctx.complete(NotFound, "The requested auction was not found in eBay's servers.")
 
-                  case sTime =>
-                    val sInfo = reqInfo.copy(auctionId = auctionId, snipeTime = sTime)
-                    registerAndActivate(new Snipe(sInfo))
-                    sInfo
+        case code if code >= 500 && code <= 599 =>
+          ctx.complete(BadGateway, "An error occurred in eBay's servers.")
+
+        case _ =>
+          ctx.complete(InternalServerError, "An internal error occurred.")
+      }
+    }
+  }
+
+  startServer("0.0.0.0", config.getInt("sniper.port")) {
+    handleExceptions(ebayHttpExceptionHandler) {
+      logServiceRequest {
+        pathPrefix("auction" / Segment) { auctionId =>
+          path("snipe") {
+            get {
+              complete {
+                snipes.get(auctionId) match {
+                  case None => NotFound -> "No snipe was defined for this auction yet."
+                  case Some(snipe) => snipe.info
+                }
+              }
+            } ~
+            post {
+              entity(as[SnipeInfo]) { reqInfo =>
+                complete {
+                  scheduler.snipeTimeFor(auctionId, reqInfo.snipeTime) match {
+                    case None => BadRequest -> "The auction has already ended."
+
+                    case sTime =>
+                      val sInfo = reqInfo.copy(auctionId = auctionId, snipeTime = sTime)
+                      registerAndActivate(new Snipe(sInfo))
+                      sInfo
+                  }
+                }
+              }
+            } ~
+            delete {
+              complete {
+                snipes.get(auctionId) match {
+                  case None => NotFound -> "No snipe was defined for this auction yet."
+                  case Some(snipe) => snipe.cancel(); snipe.info
                 }
               }
             }
           } ~
-          delete {
-            complete {
-              snipes.get(auctionId) match {
-                case None => NotFound -> "No snipe was defined for this auction yet."
-                case Some(snipe) => snipe.cancel(); snipe.info
-              }
-            }
+          pathEnd {
+            get { complete(ebay.auctionInfo(auctionId)) }
           }
         } ~
-        pathEnd {
-          get { complete(ebay.auctionInfo(auctionId)) }
+        path("snipes") {
+          get { complete(snipes.values.map(_.info)) }
         }
-      } ~
-      path("snipes") {
-        get { complete(snipes.values.map(_.info)) }
       }
     }
   }
